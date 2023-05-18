9097636d29e435e299dc8278c3e8f58bc612c7d8
jjin286 <j1jin286@gmail.com>
2023-05-18 15:39:30 -0700

    Link

--- src/frontend/src/components/SideBar.js
+++ src/frontend/src/components/SideBar.js
@@ -2,6 +2,8 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../App.css'
+import {Link} from "react-router-dom";
+
 
 export const SideBar = ({year}) => {
    
